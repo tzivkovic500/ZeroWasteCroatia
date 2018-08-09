4161156283db6c952bbd8e60953a6ed6462c284d
tea <tzivkovic500@gmail.com>
2018-08-09 20:32:00 +0200

    AboutFragment

--- app/src/main/java/com/example/android/zerowastecroatia/AboutFragment.java
+++ app/src/main/java/com/example/android/zerowastecroatia/AboutFragment.java
@@ -1,6 +1,7 @@
 package com.example.android.zerowastecroatia;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -8,6 +9,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import java.util.ArrayList;
@@ -21,8 +23,51 @@ public class AboutFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.activity_info, container, false);
-        return rootView;
+        View aboutView = inflater.inflate(R.layout.activity_info, container, false);
+        ImageButton facebookButton = aboutView.findViewById(R.id.facebook);
+        facebookButton.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View arg0)
+            {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW,Uri.parse("https://www.facebook.com/ZeroWasteCroatia/"));
+                startActivity(browserIntent);
 
+            }
+        });
+        ImageButton instagramButton = aboutView.findViewById(R.id.instagram);
+        instagramButton.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View arg0)
+            {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW,Uri.parse("https://www.instagram.com/zerowastecroatia1/"));
+                startActivity(browserIntent);
+
+            }
+        });
+        ImageButton callButton = aboutView.findViewById(R.id.call_button);
+        callButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:0038591 506 4529"));
+                 {
+                    startActivity(intent);
+                }
+            }
+        });
+        ImageButton emailButton = aboutView.findViewById(R.id.emailButton);
+        emailButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:zerowastecroatia1@gmail.com")); // only email apps should handle this
+                  {
+                    startActivity(intent);
+                }
+
+            }
+        });
+
+        return aboutView;
     }
 }

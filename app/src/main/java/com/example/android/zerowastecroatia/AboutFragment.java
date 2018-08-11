2ed2ca3795f5e96ca0193623f79cba5063c0506b
tea <tzivkovic500@gmail.com>
2018-08-11 17:02:53 +0200

    activity_info, sendButton, colors, dimens, strings, styles, AboutFragment

--- app/src/main/java/com/example/android/zerowastecroatia/AboutFragment.java
+++ app/src/main/java/com/example/android/zerowastecroatia/AboutFragment.java
@@ -1,18 +1,21 @@
 package com.example.android.zerowastecroatia;
 
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.net.Uri;
-import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v7.app.AppCompatActivity;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ImageButton;
-import android.widget.LinearLayout;
-import android.widget.ListView;
-import java.util.ArrayList;
+        import android.content.pm.PackageManager;
+        import android.net.Uri;
+        import android.os.Bundle;
+        import android.support.v4.app.Fragment;
+        import android.support.v7.app.AppCompatActivity;
+        import android.view.LayoutInflater;
+        import android.view.View;
+        import android.view.ViewGroup;
+        import android.widget.Button;
+        import android.widget.ImageButton;
+        import android.widget.LinearLayout;
+        import android.widget.ListView;
+
+        import java.util.ArrayList;
+
 /**
  * Created by Tea on 2.8.2018..
  */
@@ -20,26 +23,23 @@ import java.util.ArrayList;
 public class AboutFragment extends Fragment {
     public AboutFragment() {
     }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View aboutView = inflater.inflate(R.layout.activity_info, container, false);
         ImageButton facebookButton = aboutView.findViewById(R.id.facebook);
-        facebookButton.setOnClickListener(new View.OnClickListener()
-        {
-            public void onClick(View arg0)
-            {
-                Intent browserIntent = new Intent(Intent.ACTION_VIEW,Uri.parse("https://www.facebook.com/ZeroWasteCroatia/"));
+        facebookButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.facebook.com/ZeroWasteCroatia/"));
                 startActivity(browserIntent);
 
             }
         });
         ImageButton instagramButton = aboutView.findViewById(R.id.instagram);
-        instagramButton.setOnClickListener(new View.OnClickListener()
-        {
-            public void onClick(View arg0)
-            {
-                Intent browserIntent = new Intent(Intent.ACTION_VIEW,Uri.parse("https://www.instagram.com/zerowastecroatia1/"));
+        instagramButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.instagram.com/zerowastecroatia1/"));
                 startActivity(browserIntent);
 
             }
@@ -50,7 +50,7 @@ public class AboutFragment extends Fragment {
             public void onClick(View v) {
                 Intent intent = new Intent(Intent.ACTION_DIAL);
                 intent.setData(Uri.parse("tel:0038591 506 4529"));
-                 {
+                {
                     startActivity(intent);
                 }
             }
@@ -59,15 +59,28 @@ public class AboutFragment extends Fragment {
         emailButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-        Intent intent = new Intent(Intent.ACTION_SENDTO);
-        intent.setData(Uri.parse("mailto:zerowastecroatia1@gmail.com")); // only email apps should handle this
-                  {
+                Intent intent = new Intent(Intent.ACTION_SENDTO);
+                intent.setData(Uri.parse("mailto:zerowastecroatia1@gmail.com")); // only email apps should handle this
+                {
                     startActivity(intent);
                 }
 
             }
         });
-
+        // email message with new place
+        Button sendButton = aboutView.findViewById(R.id.sendButton);
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_SENDTO);
+                intent.setData(Uri.parse("mailto:zerowastecroatia1@gmail.com")); // only email apps should handle this
+                intent.putExtra(Intent.EXTRA_SUBJECT, "New Zero Waste place for list");
+                intent.putExtra(Intent.EXTRA_TEXT, "Hi! You want to add New Zero Waste place? Fill this application:" + "\nPlace name:" + "\nPlace address:" + "\nweb/facebook:" + "\nType (food, cosmetics...):" + "\nThank you!" + "\nZero Waste Croatia");
+                {
+                    startActivity(intent);
+                }
+            }
+        });
         return aboutView;
     }
 }
